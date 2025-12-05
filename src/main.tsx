346a5853b458afcd42f45d4055846a9ade1af7da
Kevin <kevin@switchlabs.ai>
2025-12-05 21:13:28 +0900

    fix: web3auth - login

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,16 @@
-import { StrictMode } from 'react';
-import './i18n';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import './polyfills';
-import App from './App.tsx';
-import { Web3AuthProvider } from '@web3auth/modal/react';
-import web3AuthContextConfig from './web3authContext';
-import { WagmiProvider } from '@web3auth/modal/react/wagmi';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { StrictMode } from "react";
+import "./i18n";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { Web3AuthProvider } from "@web3auth/modal/react";
+import web3AuthContextConfig from "./web3authContext";
+import { WagmiProvider } from "@web3auth/modal/react/wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root') as HTMLElement).render(
+createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <Web3AuthProvider config={web3AuthContextConfig}>
       <QueryClientProvider client={queryClient}>
--- src/web3authContext.ts
+++ src/web3authContext.ts
@@ -1,17 +1,16 @@
-import { WEB3AUTH_NETWORK } from '@web3auth/modal';
-import type { Web3AuthContextConfig } from '@web3auth/modal/react';
+import { WEB3AUTH_NETWORK } from "@web3auth/modal";
+import type { Web3AuthContextConfig } from "@web3auth/modal/react";
 
 // Web3Auth React modal configuration
 // Uses sapphire_devnet as in the official quick-start
-const clientId = import.meta.env.WEB3AUTH_CLIENT_ID;
+const clientId =
+  "BHgArYmWwSeq21czpcarYh0EVq2WWOzflX-NTK-tY1-1pauPzHKRRLgpABkmYiIV_og9jAvoIxQ8L3Smrwe04Lw"; // get from https://dashboard.web3auth.io;
 
 const web3AuthContextConfig: Web3AuthContextConfig = {
   web3AuthOptions: {
     clientId,
-    web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_DEVNET
-  }
+    web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_DEVNET,
+  },
 };
 
 export default web3AuthContextConfig;
-
-
